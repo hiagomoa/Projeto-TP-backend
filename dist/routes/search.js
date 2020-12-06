e1000d81fe6804413d22c04966bdf8c5082a891c
hiagomoa <hiagosilvafernandes@hotmail.com>
2020-12-06 20:55:21 -0300

    add

--- dist/routes/search.js
+++ dist/routes/search.js
@@ -1,4 +1,34 @@
 "use strict";
+/*import { Router } from 'express';
+
+const searchRouter = Router();
+
+import data from '../DADOS/livros.json';
+
+searchRouter.get('/', (req, res)=>{
+  const querry = req.query;
+  const {input} = querry;
+  //const newInput = input.toUpperCase;
+  
+  const result = data[0].books?.map(o => {
+    let titulo = o.titulo.toUpperCase();
+    let inputZao;
+    if(input!= undefined){
+    inputZao= input.toUpperCase();
+  }
+    let autor = o.autor.toUpperCase();
+    if (titulo == inputZao || autor == inputZao) { return o; }
+    });
+
+
+const resultFinal = (result || []).filter(function (el) {
+    return el != null;
+  });
+
+return res.status(200).json(data);
+});
+
+export default searchRouter;*/
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
@@ -8,23 +38,20 @@ var searchRouter = express_1.Router();
 var livros_json_1 = __importDefault(require("../DADOS/livros.json"));
 searchRouter.get('/', function (req, res) {
     var _a;
-    var querry = req.query;
-    var input = querry.input;
+    var input = req.query.input;
+    var newInput = input === null || input === void 0 ? void 0 : input.toString();
     //const newInput = input.toUpperCase;
     var result = (_a = livros_json_1.default[0].books) === null || _a === void 0 ? void 0 : _a.map(function (o) {
         var titulo = o.titulo.toUpperCase();
-        var inputZao;
-        if (input != undefined) {
-            inputZao = input.toUpperCase();
-        }
+        var inputZao = newInput.toUpperCase();
         var autor = o.autor.toUpperCase();
         if (titulo == inputZao || autor == inputZao) {
             return o;
         }
     });
-    var resultFinal = (result || []).filter(function (el) {
+    var resultFinal = result.filter(function (el) {
         return el != null;
     });
-    return res.status(200).json(livros_json_1.default);
+    return res.status(200).json(resultFinal[0]);
 });
 exports.default = searchRouter;
--- src/routes/search.ts
+++ src/routes/search.ts
@@ -1,5 +1,5 @@
 
-import { Router } from 'express';
+/*import { Router } from 'express';
 
 const searchRouter = Router();
 
@@ -14,7 +14,7 @@ searchRouter.get('/', (req, res)=>{
     let titulo = o.titulo.toUpperCase();
     let inputZao;
     if(input!= undefined){
-    inputZao= (input as string).toUpperCase();
+    inputZao= input.toUpperCase();
   }
     let autor = o.autor.toUpperCase();
     if (titulo == inputZao || autor == inputZao) { return o; }
@@ -26,6 +26,34 @@ const resultFinal = (result || []).filter(function (el) {
   });
 
 return res.status(200).json(data);
+});
+
+export default searchRouter;*/
+
+
+import { Router } from 'express';
+
+const searchRouter = Router();
+
+import data from '../DADOS/livros.json';
+
+searchRouter.get('/', (req, res)=>{
+  const { input } = req.query;
+  const newInput = input?.toString();
+  //const newInput = input.toUpperCase;
+  const result = data[0].books?.map(o => {
+    let titulo = o.titulo.toUpperCase();
+    let inputZao= newInput!.toUpperCase();
+    let autor = o.autor.toUpperCase();
+    if (titulo == inputZao || autor == inputZao) { return o; }
+    });
+
+
+const resultFinal = result!.filter(function (el) {
+    return el != null;
+  });
+
+return res.status(200).json(resultFinal[0]);
 });
 
 export default searchRouter;
